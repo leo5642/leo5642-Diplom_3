85e7c37d34f6b310132926c855839865edff2201
Lev Struin <lev_struin@mail.ru>
2025-08-05 19:33:11 +0500

    0

--- locators/url.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-class UrlCollector:
-    url_home = 'https://stellarburgers.nomoreparties.site/'
-    url_list_order = url_home +  'feed'
-
-    url_register = url_home + 'api/auth/register'
-    url_login = url_home + 'api/auth/login'
-    url_order = url_home + 'api/orders'
